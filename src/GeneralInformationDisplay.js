185447b6ecfdcd1f52158db83acd261855097f27
AlexCiobanu47 <alexciobanu72@yahoo.com>
2023-02-21 18:57:38 +0200

    Added form and display for general information

--- src/GeneralInformationDisplay.js
+++ src/GeneralInformationDisplay.js
@@ -1,13 +1,16 @@
 import React, {Component} from "react";
 
 class GeneralInformationDisplay extends Component{
+    constructor(props){
+        super(props);
+    }
         render(){
             return(
                 <div>
-                    <p></p>
-                    <p></p>
-                    <p></p>
-                    <p></p>
+                    <p>{this.props.firstName}</p>
+                    <p>{this.props.lastName}</p>
+                    <p>{this.props.email}</p>
+                    <p>{this.props.phone}</p>
                 </div>
             )
         }
--- src/GeneralInformationForm.js
+++ src/GeneralInformationForm.js
@@ -2,30 +2,71 @@ import React , {Component} from "react";
 import GeneralInformationDisplay from "./GeneralInformationDisplay";
 
 class GeneralInformationForm extends Component{
+    constructor(){
+        super();
 
+        this.state = {
+            firstName: '',
+            lastName: '',
+            email: '',
+            phone: '',
+        };
+    }
+    handleFirstNameChange =(e) => {
+        this.setState({
+            firstName: e.target.value,
+        })
+    };
+    handleLastNameChange =(e) => {
+        this.setState({
+            lastName: e.target.value,
+        })
+    };
+    handleEmailChange =(e) => {
+        this.setState({
+            email: e.target.value,
+        })
+    };
+    handlePhoneChange =(e) => {
+        this.setState({
+            phone: e.target.value,
+        })
+    };
+    onSubmitInfo = (e) =>{
+        e.preventDefault();
+    }
     render(){
         return(
             <div>
-                <form>
+                <form onSubmit={this.onSubmitInfo}>
                     <label htmlFor="">First name:</label>
                     <input
                         type = 'text'
+                        onChange={this.handleFirstNameChange}
                     />
                     <label htmlFor="">Last name:</label>
                     <input
                         type = 'text'
+                        onChange={this.handleLastNameChange}
                     />
                     <label htmlFor="">Email:</label>
                     <input
                         type = 'email'
+                        onChange={this.handleEmailChange}
                     />
                     <label htmlFor="">Phone number:</label>
                     <input
                         type = 'text'
+                        onChange={this.handlePhoneChange}
                     />
                     <button type = 'submit'>Submit</button>
                 </form>
-                <GeneralInformationDisplay/>
+                <GeneralInformationDisplay 
+                    firstName = {this.state.firstName}
+                    lastName = {this.state.lastName}
+                    email = {this.state.email}
+                    phone = {this.state.phone}
+                />
             </div>
         )
     }
